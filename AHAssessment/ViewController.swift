ff2a7725743db07d3e02b988a5c21dd4bcf713c3
Sergei Podnebesnyi (EPAM) <sergei_podnebesnyi@epam.com>
2023-04-18 18:33:12 +0600

    Initial project setup

---
+++ AHAssessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AHAssessment
+//
+//  Created by Sergei Podnebesnyi on 18.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
